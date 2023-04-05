c5ddf0636df6ba866a182668d6481e7ab0d7e808
Samuel <samuel.estudo778@gmail.com>
2023-04-05 10:30:00 -0300

    feat: create Section component

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,11 +1,24 @@
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
-import { Container } from "./styles"
+import { Section } from "../../components/Section"
+import { Container, Links } from "./styles"
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis" >
+        <Links>
+          <li><a href="#">https://github.com/samuelfsd</a></li>
+          <li><a href="#">https://github.com/samuelfsd</a></li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores" >
+        
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   )
