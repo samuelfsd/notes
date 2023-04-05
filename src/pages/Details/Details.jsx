7af033eb63f37e2b925b8a2d3ec186947e4c9d76
Samuel <samuel.estudo778@gmail.com>
2023-04-05 10:49:29 -0300

    feat: create ButtonText

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,4 +1,5 @@
 import { Button } from "../../components/Button"
+import { ButtonText } from "../../components/ButtonText"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
@@ -8,6 +9,7 @@ export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Excluir nota" />
 
       <Section title="Links úteis" >
         <Links>
