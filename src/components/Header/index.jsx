43fac284cc6de5175ba78b4bb5584122f0c31922
Samuel <samuel.estudo778@gmail.com>
2023-04-05 10:09:14 -0300

    feat: create Header and install react-icons lib

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import { RiShutDownLine } from 'react-icons/ri';
+import { Container, Profile, Logout} from "./styles";
+
+
+
+export function Header(){
+  return (
+    <Container>
+      <Profile>
+        <img 
+          src="https://github.com/samuelfsd.png" 
+          alt="Foto do usuário" 
+        />
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>Samuel Alves</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
+}
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,11 +1,11 @@
 import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
 import { Container } from "./styles"
 
 export function Details() {
   return (
     <Container>
-      <h1>details</h1>
-      <h3>bora porra</h3>
+      <Header />
       <Button title="Voltar" />
     </Container>
   )
