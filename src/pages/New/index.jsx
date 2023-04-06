cfde644f641a1a0d4406728c20c61c012a31559e
Samuel <samuel.estudo778@gmail.com>
2023-04-06 15:01:32 -0300

    feat: create New note page

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Container, Form } from './styles';
+
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
+
+export function New() {
+  return (
+    <Container>
+        <Header />
+
+        <main>
+          <Form>
+            <header>
+              <h1>Criar nota</h1>
+              <a href="/">Voltar</a>
+            </header>
+
+            <Input placeholder="Título" />
+            <Textarea placeholder="Observações" />
+          </Form>
+        </main>
+    </Container>
+  );
+} 
